63e7913d096b44cb00830885d67b98bc1ad1b279
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-03-02 11:07:07 -0500

    getting html from page

--- scrape.py
+++ scrape.py
@@ -1,2 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+
+
+page = requests.get('https://www.nascar.com/news/nascar-cup-series/').text
+
+soup = BeautifulSoup(page, 'lxml')
+
+print(soup.prettify())
