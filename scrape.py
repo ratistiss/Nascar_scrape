9d26631e4babfb33542531cab227f021a098717e
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-03-03 11:35:54 -0500

    failing email Auth

--- scrape.py
+++ scrape.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 page = requests.get('https://www.nascar.com/news/nascar-cup-series/').text
@@ -18,3 +21,45 @@ for videos in video:
     print(caption)
     print(url)
 
+sender_email = "testmydev206@gmail.com"
+receiver_email = "manderson3600@gmail.com"
+password = input("Type your password and press enter:")
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+
+text = """\
+Hi,
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
+
+
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+
+message.attach(part1)
+message.attach(part2)
+
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
